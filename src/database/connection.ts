0b04b99b7d6713ac4ff3c3ec9816c54c8cd8492c
Natalia Manquillo <manquillonatalia04@gmail.com>
2024-08-06 23:28:53 -0500

    se actualiza conexiónde a base de datos

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,8 +3,8 @@ import Usuario from "../models/usuario";
 import Tarea from "../models/tarea";
 import Estado from "../models/estado";
 
-const database = new Sequelize ('gestiontareas', 'root', '', {
-    host: 'mysql',
+const database = new Sequelize ('ryfq55d728nsmpm4', 'bb8pkkqim9zp3if8', 'pe0uborn7t6cc1s5', {
+    host: 'tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
 
 
