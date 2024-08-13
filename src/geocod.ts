c7de7b2adaab18005e80fe9f0613f5eec20fd0f5
Natalia Manquillo <manquillonatalia04@gmail.com>
2024-08-12 22:35:10 -0500

    Refactorización de código

--- src/geocod.ts
+++ src/geocod.ts
@@ -5,8 +5,6 @@ import { json } from 'express';
 const BASE_URL="https://maps.googleapis.com/maps/api/geocode/"
 
 
-
-
 export const instance = axios.create({
    baseURL: BASE_URL
 
@@ -15,30 +13,24 @@ export const instance = axios.create({
 export async function getUbicacion(direccion: string, country:string, state:string, city:string) {
 
     const apikey='AIzaSyBZlJJ_dE5_OEKTuIJMDbIf5ohc_TMba7s'
+
     var location= {};
 
     //`1600+Amphitheatre+Parkway,+Mountain+View,+CA`
     const geocoderQuery = encodeURIComponent(direccion+city+state+country.replace(/ /g, '+'))
 
-    const response = await instance.get(`json?address=`+`${geocoderQuery}`+`&key=`+`${apikey}`)
+    await instance.get(`json?address=`+`${geocoderQuery}`+`&key=`+`${apikey}`)
+    //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBZlJJ_dE5_OEKTuIJMDbIf5ohc_TMba7s
     //guarda en res
     //res.data están los datos
     .then(res => res.data)
-    //transforma en un formato json, respuesta de la api
-    //accedder a un formato donde pueda acceder más fácil a los datos
-    .then(json => {
-      if (json.results.length === 0) {
+
+    .then(res => {
+      if (res.results.length === 0) {
         return null
       }
 
-      //
-      const lat = json.results['0'].geometry.location.lat
-      const lng = json.results['0'].geometry.location.lng
-
-      console.log(json.results['0'].geometry.location);
-
-
-      location =  json.results['0'].geometry.location
+      location =  res.results['0'].geometry.location
       
     });
 
